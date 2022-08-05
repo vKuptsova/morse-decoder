f7e93a89a36cddd2a4a995897a891fa77fd69160
vKuptsova <kuptsowa.vika@gmail.com>
2022-08-05 23:23:58 +0300

    feat: add code to index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let arr2 = [];
+    let t = expr.length / 10;
+    let pos = 0;
+    const iterator = Object.keys(MORSE_TABLE);
+    for (let i = 0; i<t; i++) {
+        arr[i] = expr.substring(10+pos, pos);
+        pos +=10;
+        let p = 0;
+            let a = arr[i];
+            if (a != '**********') { 
+              for (let j = 0; j<5; j++) {
+                if ( a.substring(2+p, p) === '10') {
+                    arr2[j] = ".";
+                    p += 2;
+                } else if ( a.substring(2+p, p) === '11'){
+                    arr2[j] = "-";
+                    p += 2;
+                } else if ( a.substring(2+p, p) === '00'){
+                    arr2[j] = "";
+                    p += 2;
+                }
+                
+            } 
+            arr[i] = arr2.join('');
+        } else {
+                arr[i] = " ";
+            }
+        for (const key of iterator) {
+        if (arr[i] === key){
+           arr[i] = MORSE_TABLE[key];
+        }
+    } 
+  }
+    return arr.join("");
 }
 
 module.exports = {
